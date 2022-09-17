18631c1e3ba1eb3f0f5bf3108e1b931aa1461d81
Carmen <Kanpii@Gmail.com>
2022-09-16 22:25:15 -0500

    fixed some typos

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -17,15 +17,15 @@ function MainPage() {
       <div className="py-1 text-center" id="mainContainer">
         <div
           id="carouselExampleCaptions"
-          class="carousel slide"
+          className="carousel slide"
           data-bs-ride="false"
         >
-          <div class="carousel-indicators">
+          <div className="carousel-indicators">
             <button
               type="button"
               data-bs-target="#carouselExampleCaptions"
               data-bs-slide-to="0"
-              class="active"
+              className="active"
               aria-current="true"
               aria-label="Slide 1"
             ></button>
@@ -42,64 +42,64 @@ function MainPage() {
               aria-label="Slide 3"
             ></button>
           </div>
-          <div class="carousel-inner">
-            <div class="carousel-item active">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
               <img
                 src="https://source.unsplash.com/2400x1200/?mercedes"
-                class="d-block w-100"
+                className="d-block w-100"
                 alt="slider1: capable"
               />
 
-              <div class="carousel-caption d-none d-md-block bg-gray">
+              <div className="carousel-caption d-none d-md-block bg-gray">
                 <h3>Test drives for real life</h3>
                 <p>24-hour take home test drives</p>
               </div>
             </div>
-            <div class="carousel-item">
+            <div className="carousel-item">
               <img
                 src="https://source.unsplash.com/2400x1200/?car"
-                class="d-block w-100"
+                className="d-block w-100"
                 alt="slider2: adaptive"
               />
-              <div class="carousel-caption d-none d-md-block">
+              <div className="carousel-caption d-none d-md-block">
                 <h3>Love it or return it</h3>
                 <p>30-day money back (up to 1500 mi.)</p>
               </div>
             </div>
-            <div class="carousel-item">
+            <div className="carousel-item">
               <img
                 src="https://source.unsplash.com/2400x1200/?bmw"
-                class="d-block w-100"
+                className="d-block w-100"
                 alt="slider3: accountable"
               />
 
-              <div class="carousel-caption d-none d-md-block">
+              <div className="carousel-caption d-none d-md-block">
                 <h3>All major syestems covered</h3>
                 <p>90 days or 4,000 miles (whichever comes first)</p>
               </div>
             </div>
           </div>
           <button
-            class="carousel-control-prev"
+            className="carousel-control-prev"
             type="button"
             data-bs-target="#carouselExampleCaptions"
             data-bs-slide="prev"
           >
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
           </button>
           <button
-            class="carousel-control-next"
+            className="carousel-control-next"
             type="button"
             data-bs-target="#carouselExampleCaptions"
             data-bs-slide="next"
           >
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
           </button>
         </div>
 
-        <footer class="bg-dark text-light text-center fixed-bottom py-3">
+        <footer className="bg-dark text-light text-center fixed-bottom py-3">
           © 2022 Copyright: CarCar | All rights reserved
         </footer>
       </div>
